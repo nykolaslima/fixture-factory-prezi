4370fd3c19d57f5baa7c0c0ddc35cf05f8b506cb
Nykolas Lima <nykolas.lima@gmail.com>
2014-11-15 17:36:02 -0200

    adding simple test example

---
+++ src/test/java/com/vivareal/fixtureFactoryPrezi/beginWithTests/SimpleTests.java
@@ -0,0 +1,14 @@
+package com.vivareal.fixtureFactoryPrezi.beginWithTests;
+
+import static junit.framework.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class SimpleTests {
+
+	@Test
+	public void shouldAddTwoNumbers() {
+		assertEquals(2, 1 + 1);
+	}
+	
+}
